54571343c0b87b281f8f756537947fbddda5e9ef
Wisdom Ochei <wisdomofgod2003@gmail.com>
2022-08-22 10:18:38 +0100

    Design section has been added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const burgerOpen = document.querySelectorAll(".activator-buttons img")[0];
 const burgerClose = document.querySelectorAll(".activator-buttons img")[1];
 const dropdown = document.querySelector("#dropdowns-div");
+const connect = document.querySelector("#connect");
+const connectArrow = document.querySelector("#connect img");
+const summary = document.querySelector("summary");
 
 // To open mobile nav
 burgerOpen.addEventListener("click", () => {
@@ -17,4 +20,8 @@ burgerClose.addEventListener("click", () => {
     burgerClose.classList.toggle("inactive");
     dropdown.classList.toggle("inactive");
     burgerOpen.classList.remove("inactive");
+})
+
+connect.addEventListener, summary.addEventListener("click", () => {
+    connectArrow.classList.toggle("rotate-arrow");
 })
